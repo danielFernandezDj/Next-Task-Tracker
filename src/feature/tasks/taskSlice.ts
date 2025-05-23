f34512d88f6711c7371fc68e32f760a257dc3466
Daniel Fernandez <mdanif9728@gmail.com>
2025-05-23 14:01:39 -0700

    Modifying the taskSlice.ts

--- src/feature/tasks/taskSlice.ts
+++ src/feature/tasks/taskSlice.ts
@@ -1,6 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
-    value: 0,
-    state: "idle",
+interface Task {
+  id: string;
+  title: string;
+  completed: string;
 }
+
+interface TaskState {
+  tasks: Task[];
+  status: "idle" | "loading" | "failed";
+}
+
+const initialState: TaskState = {
+  tasks: [],
+  status: "idle",
+};
+
+//  addTask, removeTask, setTasks.
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState,
+  reducers: {
+    addTask(state, action: PayloadAction<Task>) {
+      state.tasks.push(action.payload);
+    },
+    removeTask(state, action: PayloadAction<string>) {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    },
+    setTasks(state, action: PayloadAction<Task[]>) {
+      state.tasks = action.payload;
+    },
+  },
+});
+
+export const { addTask, removeTask, setTasks } = taskSlice.actions;
+export default taskSlice.reducer;
